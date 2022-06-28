eb13d2b67215e32ba71a31abb879ef8ad6b8a72f
gpotje <gpotje@github.com>
2022-06-27 21:46:10 -0300

    endpont's save e findById

--- src/main/java/com/example/demo/controller/AnimeController.java
+++ src/main/java/com/example/demo/controller/AnimeController.java
@@ -3,8 +3,15 @@ package com.example.demo.controller;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Anime;
@@ -25,9 +32,19 @@ public class AnimeController {
 	private final AnimeService service;
 	
 	@GetMapping
-	public List<Anime> list(){
+	public ResponseEntity<List<Anime>> list(){
 		log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-		return service.listAll();
+		return ResponseEntity.ok(service.listAll());
 	}	
 
+	@GetMapping("/{id}")
+	public ResponseEntity<Anime> FindById(@PathVariable Long id){
+		log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+		return ResponseEntity.ok(service.findById(id));
+	}	
+	
+	@PostMapping
+	public ResponseEntity<Anime> save(@RequestBody Anime anime){
+		return new ResponseEntity<>(service.save(anime),HttpStatus.CREATED);
+	}
 }
--- src/main/java/com/example/demo/service/AnimeService.java
+++ src/main/java/com/example/demo/service/AnimeService.java
@@ -1,16 +1,38 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.demo.domain.Anime;
 
 @Service
 public class AnimeService {
+
+	private static List<Anime> animes;
+
+	static {
+		animes = new ArrayList<>(
+				List.of(new Anime(1l, "Naruto"), new Anime(2l, "yu-gi-oh"), new Anime(3l, "yu yu haskusho")));
+	}
+
+	public List<Anime> listAll() {
+		return animes;
+	}
+
+	public Anime findById(Long id) {
+		return animes.stream().filter(anime -> anime.getId().equals(id)).findFirst()
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+	}
 	
-	public List<Anime> listAll(){
-		return List.of(new Anime(1l,"Naruto"), new Anime(2l,"yu-gi-oh"), new Anime(3l,"yu yu haskusho"));
+	public Anime save(Anime anime) {
+		anime.setId(ThreadLocalRandom.current().nextLong(3,10000));
+		animes.add(anime);
+		return anime;
 	}
 
 }
